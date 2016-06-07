09a04598fcbbccaac9bd3b8ce2b87205a1ff8734
Marc Harter <wavded@gmail.com>
2016-06-07 11:17:36 -0500

    Switch to tape for tests

--- test/scaryshot-test.js
+++ test/scaryshot-test.js
@@ -1,102 +1,100 @@
 var request = require('supertest')
+var test = require('tape')
 var fs = require('fs')
-var assert = require('assert')
 var app = require('../')
-var sampleUrlWithProtocol = "http://www.google.com/"
-var sampleUrl = "www.google.com"
+var sampleUrlWithProtocol = 'http://www.google.com/'
+var sampleUrl = 'www.google.com'
 
-describe('/generate', function () {
-  this.timeout(60000)
+function end(t) {
+  return function(er) {
+    t.notOk(er)
+    t.end()
+  }
+}
 
-  it('should fail if no url or html param supplied', function (done) {
+test('/generate', function(ts) {
+  ts.test('should fail if no url or html param supplied', function(t) {
     request(app)
       .get('/generate')
       .expect('content-type', 'application/json')
-      .expect(400)
-      .end(function (er, res) {
-        if (er) return done(er)
-
-        assert.equal(res.body.status, 400, 'expected 400 status property')
-        assert.ok(res.body.message, 'expected an error message')
-        done()
+      .end(function(er, res) {
+        t.equal(res.body.status, 400, 'expected 400 status property')
+        t.ok(res.body.message, 'expected an error message')
+        t.end()
       })
   })
-  it('should fail if invalid type param supplied', function (done) {
+
+  ts.test('should fail if invalid type param supplied', function(t) {
     request(app)
       .post('/generate')
       .type('form')
-      .send({ url: 'google.com', type: 'tiff' })
-      .expect('content-type', 'application/json')
-      .expect(400)
-      .end(function (er, res) {
-        if (er) return done(er)
-
-        assert.equal(res.body.status, 400, 'expected 400 status property')
-        assert.ok(/tiff/.test(res.body.message), 'expected tiff within error message')
-        done()
+      .send({url: 'google.com', type: 'tiff'})
+      .end(function(er, res) {
+        t.equal(res.body.status, 400, 'expected 400 status property')
+        t.ok(/tiff/.test(res.body.message), 'expected tiff within error message')
+        t.end()
       })
   })
-  it('should accept url with protocol', function (done) {
+
+  ts.test('should accept url with protocol', function(t) {
     request(app)
       .get('/generate')
-      .query({ url: sampleUrlWithProtocol })
+      .query({url: sampleUrlWithProtocol})
       .expect('content-type', 'application/pdf')
-      .expect(200, done)
+      .expect(200, end(t))
   })
-  it('should accept url without protocol', function (done) {
+
+  ts.test('should accept url without protocol', function(t) {
     request(app)
       .post('/generate')
       .type('form')
-      .send({ url: sampleUrl })
+      .send({url: sampleUrl})
       .expect('content-type', 'application/pdf')
-      .expect(200, done)
+      .expect(200, end(t))
   })
-  it('should generate png files', function (done) {
+  ts.test('should generate png files', function(t) {
     request(app)
       .get('/generate')
-      .query({ url: sampleUrl, type: 'png' })
+      .query({url: sampleUrl, type: 'png'})
       .expect('content-type', 'image/png')
-      .expect(200, done)
+      .expect(200, end(t))
   })
-  it('should generate gif files', function (done) {
+  ts.test('should generate gif files', function(t) {
     request(app)
       .post('/generate')
       .type('form')
-      .send({ url: sampleUrl, type: 'gif' })
+      .send({url: sampleUrl, type: 'gif'})
       .expect('content-type', 'image/gif')
-      .expect(200, done)
+      .expect(200, end(t))
   })
-  it('should generate jpeg files', function (done) {
+  ts.test('should generate jpeg files', function(t) {
     request(app)
       .get('/generate')
-      .query({ url: sampleUrl, type: 'jpeg' })
+      .query({url: sampleUrl, type: 'jpeg'})
       .expect('content-type', 'image/jpeg')
-      .expect(200, done)
+      .expect(200, end(t))
   })
-  it('should generate jpg files', function (done) {
+  ts.test('should generate jpg files', function(t) {
     request(app)
       .post('/generate')
       .type('form')
-      .send({ url: sampleUrl, type: 'jpg' })
+      .send({url: sampleUrl, type: 'jpg'})
       .expect('content-type', 'image/jpeg')
-      .expect(200, done)
+      .expect(200, end(t))
   })
-  it('should generate output from html', function (done) {
+  ts.test('should generate output from html', function(t) {
     request(app)
       .get('/generate')
-      .query({ html: '<h1>Hello World</h1>', type: 'png' })
+      .query({html: '<h1>Hello World</h1>', type: 'png'})
       .expect('content-type', 'image/png')
-      .expect(200, done)
+      .expect(200, end(t))
   })
-  it('should handle large html input', function (done) {
+  ts.test('should handle large html input', function(t) {
     request(app)
       .post('/generate')
       .type('form')
-      .send({ html: fs.readFileSync(__dirname+'/fixtures/large.html').toString(), type: 'png' })
-      // .expect('content-type', 'image/png')
-      .expect(200, function (er, res) {
-        console.log(res.body)
-        done()
-      })
+      .send({html: fs.readFileSync(__dirname + '/fixtures/large.html').toString(), type: 'png'})
+      .expect('content-type', 'image/png')
+      .expect(200, end(t))
   })
 })
